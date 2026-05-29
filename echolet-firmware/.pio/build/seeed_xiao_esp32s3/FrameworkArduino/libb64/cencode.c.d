17f14a1412b6b096f11c99fb32662f557ac278cd
Artem <nasirbratan@gmail.com>
2026-05-29 13:51:17 +0300

    Remove PlatformIO build artifacts

--- echolet-firmware/.pio/build/seeed_xiao_esp32s3/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/seeed_xiao_esp32s3/FrameworkArduino/libb64/cencode.c.o: \
- /home/artem/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- /home/artem/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
